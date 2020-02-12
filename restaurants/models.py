0ee7fba3d4fe2963aae4c3a31ec60285940636f5
hammam <h.besaiso95@gmail.com>
2020-02-12 16:09:14 +0200

    added models

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Restaurant(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    opening_time = models.TimeField(auto_now=False, auto_now_add=False )
+    closing_time = models.TimeField(auto_now=False, auto_now_add=False)
 
+    def __str__(self):
+        return self.name
 # Create your models here.
